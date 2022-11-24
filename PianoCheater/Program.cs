a03e6c5605a3ac36e5207497d876965666294007
Cod3rMax <dzxxxw@gmail.com>
2022-11-24 03:47:53 +0100

    Piano Cheater Bot initial project

---
+++ PianoCheater/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PianoCheater.Helper;
+
+namespace PianoCheater
+{
+    internal class Program
+    {
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int x, int y);
+
+
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Keys xKeys);
+
+
+        static void Main(string[] args)
+        {
+
+            Helper.GetWindowRectInfo.GetRectOfTheWindow(Helper.WindowFinder.GetPianoWindow());
+            
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.X) < 0)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        if (Functions.PianoTilesChecker.PianoLocationChecker(
+                                Helper.GetWindowRectInfo.rect.Left + 68 + x * 128, GetWindowRectInfo.rect.Bottom - 420))
+                        {
+                            SetCursorPos(Helper.GetWindowRectInfo.rect.Left + 68 + x * 128, GetWindowRectInfo.rect.Bottom - 380);
+                            Helper.MouseClickHelper.MouseClick();
+                        }
+                    }
+                }
+                Thread.Sleep(2);
+            }
+
+        }
+
+
+
+    }
+}
